32e26feaeeaebe02ed96f339c0f4f48b84dd4d6a
OnanThairu <thairuonan@gmail.com>
2022-07-19 10:59:32 +0300

    Make fetch request using useEffect

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,23 @@
 // create your App component here
+import React, { useEffect, useState } from 'react'
+
+function App() {
+    const [dogImage, setDogImages] = useState("")
+    const [isLoaded, setIsLoaded] = useState(false)
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then((response) => response.json())
+        .then((data) => {
+            setDogImages(() => data.message)
+            setIsLoaded(true)
+        })
+    }, [])
+    if(!isLoaded) return <p>Loading...</p>
+
+    return (
+        <img alt='A Random Dog' src={dogImage}></img>
+    )
+}
+
+export default App
